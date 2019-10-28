7b2ad9b3ce420156d2c094d78d3b5664fb71dc28
Daniel Momot <36703311+daniel-momot@users.noreply.github.com>
2019-10-28 11:28:59 +0300

    Update output_lib.py

--- task1/lib/output_lib.py
+++ task1/lib/output_lib.py
@@ -6,7 +6,6 @@ def do_output(users, movies, grades_list, filename):
             print('  {', file=f)
             print('     "user": ' + str(user_num) + ',', file=f)
             print('     "1": {', file=f)
-            print(grades_list[user_num])
             for i, grade in enumerate(grades_list[user_num]):
                 if i != len(grades_list[user_num]) - 1:
                     print('          "' + movies[grade[0]] + '": ' + str(round(grade[1], 1)) + ',', file=f)
